786c1866f696b7166f142d75a72413ee88f9fa7c
Jeynald Endaya <jjlendaya@gmail.com>
2026-01-14 10:10:10 +0800

    feat: add serializers for organizations and organization returns

---
+++ irs_returns/rest_api/serializers/organizations/returns.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from organizations.models import OrganizationReturnInformation
+
+
+class OrganizationReturnInformationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrganizationReturnInformation
+        fields = [
+            "id",
+            "organization",
+            "filed_on",
+            "tax_period_start_date",
+            "tax_period_end_date",
+            "employee_count",
+            "total_revenue",
+            "total_expenses",
+            "total_assets",
+        ]
