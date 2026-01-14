17dd4ebdf47db8aec503425d58a4107a27769f59
Jeynald Endaya <jjlendaya@gmail.com>
2026-01-14 09:49:48 +0800

    feat: replace graphql_api with rest_api

--- irs_returns/core/settings/base.py
+++ irs_returns/core/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
-    "graphql_api",
+    "rest_api",
 ]
 
 MIDDLEWARE = [
--- irs_returns/graphql_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GraphqlApiConfig(AppConfig):
-    name = 'graphql_api'
