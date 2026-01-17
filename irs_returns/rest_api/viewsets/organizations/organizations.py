77dffc206bf1748cd4bc0c7506056a3f04675115
Jeynald Endaya <jjlendaya@gmail.com>
2026-01-18 01:00:05 +0800

    feat(api): implement read-only organization viewset

---
+++ irs_returns/rest_api/viewsets/organizations/organizations.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from organizations.models import Organization
+from rest_api.serializers.organizations.organizations import OrganizationSerializer
+
+
+class OrganizationViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
